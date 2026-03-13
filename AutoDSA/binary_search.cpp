d0274822752a766f63b6c253107cf564d43cb2ed
Akmal Malik <akmalmalik6899@gmail.com>
2026-03-13 10:11:41 +0530

    Add solution for Binary search

---
+++ AutoDSA/binary_search.cpp
@@ -0,0 +1,32 @@
+// Problem: Binary search
+// ------------------------------------------------------------
+// Problem Description:
+// Given a sorted array of distinct integers and a target value, return the index of the target if it is found in the array, otherwise return -1.
+// You must write an algorithm with O(log n) runtime complexity.
+// ------------------------------------------------------------
+// Approach: Binary Search
+// Binary search is an efficient algorithm that repeatedly divides the search interval in half. If the value of the search key is less than the item in the middle of the interval, narrow the interval to the lower half. Otherwise, narrow it to the upper half. Repeatedly check until the value is found or the interval is empty.
+// ------------------------------------------------------------
+// Time Complexity: O(log n)
+// Space Complexity: O(1)
+// ------------------------------------------------------------
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
