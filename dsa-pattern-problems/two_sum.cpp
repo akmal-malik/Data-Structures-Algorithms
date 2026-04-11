00f4c05993858dbf583a103c4cc69c4a088e7e1f
Akmal Malik <akmalmalik6899@gmail.com>
2026-04-11 09:25:11 +0530

    Add solution for Two Sum

---
+++ dsa-pattern-problems/two_sum.cpp
@@ -0,0 +1,30 @@
+// Problem: Two Sum
+// ------------------------------------------------------------
+// Problem Description:
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+// ------------------------------------------------------------
+// Approach: Hash Map
+// Iterate through the array while storing each number and its index in a hash map. For each number, check if the complement (target - current number) exists in the hash map. If it does, return the indices of the two numbers.
+// ------------------------------------------------------------
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+// ------------------------------------------------------------
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> numMap;
+        for (int i = 0; i < nums.size(); ++i) {
+            int complement = target - nums[i];
+            if (numMap.find(complement) != numMap.end()) {
+                return {numMap[complement], i};
+            }
+            numMap[nums[i]] = i;
+        }
+        return {};
+    }
+};
