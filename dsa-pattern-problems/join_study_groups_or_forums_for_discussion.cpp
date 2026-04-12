91ab8087e6c517d33df2b2e7b436ef5370357154
Akmal Malik <akmalmalik6899@gmail.com>
2026-04-12 10:02:39 +0530

    Add solution for Join study groups or forums for discussion

---
+++ dsa-pattern-problems/join_study_groups_or_forums_for_discussion.cpp
@@ -0,0 +1,30 @@
+// Problem: Two Sum
+// ------------------------------------------------------------
+// Problem Description:
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+// ------------------------------------------------------------
+// Approach: Hash Table
+// Use a hash table to store the value and its index as we iterate through the array. For each number, check if the difference between the target and the current number exists in the hash table. If it does, return the indices of the two numbers. Otherwise, add the current number and its index to the hash table.
+// ------------------------------------------------------------
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+// ------------------------------------------------------------
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> numMap;
+        for (int i = 0; i < nums.size(); ++i) {
+            int complement = target - nums[i];
+            if (numMap.find(complement) != numMap.end()) {
+                return {numMap[complement], i};
+            }
+            numMap[nums[i]] = i;
+        }
+        return {};
+    }
+};
